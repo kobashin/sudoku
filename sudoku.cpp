369052f83c7d3aca32df5d289a2c051e1499769f
genuine <shin5884genuine@outlook.com>
2021-11-28 16:07:24 +0900

    filled 61 cells

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 #include "sudoku.h"
 
-void initCandidates(int table[9][9], int candidates[9][9][9]){
+void initCandidates(int table[9][9], int fixed[9][9], int candidates[9][9][9]){
     int row;
     int column;
     int index;
@@ -15,6 +16,96 @@ void initCandidates(int table[9][9], int candidates[9][9][9]){
                         candidates[row][column][index] = 0;
                     }
                 }
+                fixed[row][column] = 1;
+            }
+        }
+    }
+}
+
+void filterRowCandidates(int row, int column, int table[9][9], int fixed[9][9], int candidates[9][9][9]){
+    int columnIndex;
+
+    for (columnIndex = 0; columnIndex < 9; columnIndex++){
+        if (columnIndex != row){
+            if (candidates[row][columnIndex][table[row][column] - 1] == table[row][column]){
+                candidates[row][columnIndex][table[row][column] - 1] = 0;
+            }
+        }
+    }
+}
+
+void filterColumnCandidates(int row, int column, int table[9][9], int fixed[9][9], int candidates[9][9][9]){
+    int rowIndex;
+
+    for (rowIndex = 0; rowIndex < 9; rowIndex++){
+        if (rowIndex != column){
+            if (candidates[rowIndex][column][table[row][column] - 1] == table[row][column]){
+                candidates[rowIndex][column][table[row][column] - 1] = 0;
+            }
+        }
+    }
+}
+
+void filterSubtableCandidates(int row, int column, int table[9][9], int fixed[9][9], int candidates[9][9][9]){
+    int rowIndex;
+    int columnIndex;
+
+    for (rowIndex = div(row, 3).quot; rowIndex < div(row, 3).quot + 3; rowIndex++){
+        for (columnIndex = div(column, 3).quot; columnIndex < div(column, 3).quot + 3; columnIndex++){
+            if((rowIndex != row) | (columnIndex != column)){
+                if (candidates[rowIndex][columnIndex][table[row][column] - 1] == table[row][column]){
+                    candidates[rowIndex][columnIndex][table[row][column] - 1] = 0;
+                }
+            }
+        }
+    }
+}
+
+void filterCandidates(int table[9][9], int fixed[9][9], int candidates[9][9][9]){
+    int row;
+    int column;
+    int index;
+
+    for (row = 0; row < 9; row++){
+        for (column = 0; column < 9; column++){
+            if (table[row][column] != 0){
+                filterRowCandidates(row, column, table, fixed, candidates);
+                filterColumnCandidates(row, column, table, fixed, candidates);
+                filterSubtableCandidates(row, column, table, fixed, candidates);
+            }
+        }
+    }
+}
+
+int countCandidates(int candidates[9]){
+    int counter = 0;
+    int index;
+
+    for (index = 0; index < 9; index++){
+        if (candidates[index] != 0){
+            counter++;
+        }
+    }
+
+    return counter;
+}
+
+void fillTable(int table[9][9], int fixed[9][9], int candidates[9][9][9]){
+    int row;
+    int column;
+    int index;
+    int value = -1;
+
+    for (int row = 0; row < 9; row++){
+        for (int column = 0; column < 9; column++){
+            if ((fixed[row][column] == 0) & (countCandidates(candidates[row][column]) == 1)){
+                for (index = 0; index < 9; index++){
+                    if (candidates[row][column][index] != 0){
+                        value = candidates[row][column][index];
+                    }
+                }
+                table[row][column] = value;
+                fixed[row][column] = 1;
             }
         }
     }
@@ -27,6 +118,17 @@ void SudokuTable::getTable(){
         }
         cout << "\n";
     }
+    cout << "\n";
+}
+
+void SudokuTable::getFixed(){
+    for (int row = 0; row < 9; row++){
+        for (int column = 0; column < 9; column++){
+            cout << this->tableInfo.fixed[row][column] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
 }
 
 void SudokuTable::getCandidates(){
@@ -47,6 +149,18 @@ void SudokuTable::getCandidates(){
     }
 }
 
+void SudokuTable::countFilled(){
+    int filled = 0;
+    for (int row = 0; row < 9; row++){
+        for (int column = 0; column < 9; column++){
+            if (this->tableInfo.table[row][column] != 0){
+                filled++;
+            }
+        }
+    }
+    cout << "filled : " << filled << endl;
+}
+
 int main(){
     tableInfo tableinfo = 
     {
@@ -60,6 +174,18 @@ int main(){
             {0, 2, 8, 3, 4, 0, 1, 0, 0},
             {3, 9, 6, 1, 5, 8, 0, 4, 7},
             {0, 1, 7, 6, 2, 9, 8, 0, 5}
+        },
+
+        {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0}
         },
 
         {
@@ -169,9 +295,37 @@ int main(){
 
     testTable.tableInfo = tableinfo;
 
-    initCandidates(testTable.tableInfo.table, testTable.tableInfo.candidates);
+    initCandidates(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    
+    filterCandidates(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    fillTable(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    testTable.countFilled();
 
+    filterCandidates(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    fillTable(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    testTable.countFilled();
+
+    filterCandidates(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    fillTable(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    testTable.countFilled();
+
+    filterCandidates(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    fillTable(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    testTable.countFilled();
+
+    filterCandidates(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    fillTable(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    testTable.countFilled();
+
+    filterCandidates(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    fillTable(testTable.tableInfo.table, testTable.tableInfo.fixed, testTable.tableInfo.candidates);
+    testTable.countFilled();
+
+    testTable.getTable();
+    /*
+    testTable.getFixed();
     testTable.getCandidates();
+    */
 
     return 0;
 }
--- sudoku.h
+++ sudoku.h
@@ -1,7 +1,14 @@
-void initCandidates(int table[9][9], int candidates[9][9][9]);
+void initCandidates(int table[9][9], int fixed[9][9], int candidates[9][9][9]);
+void filterCandidates(int table[9][9], int fixed[9][9], int candidates[9][9][9]);
+void filterRowCandidates(int row, int column, int table[9][9], int fixed[9][9], int candidates[9][9][9]);
+void filterColumnCandidates(int row, int column, int table[9][9], int fixed[9][9], int candidates[9][9][9]);
+void filterSubtableCandidates(int row, int column, int table[9][9], int fixed[9][9], int candidates[9][9][9]);
+int countCandidates(int candidates[9]);
+void fillTable(int table[9][9], int fixed[9][9], int candidates[9][9][9]);
 
 struct tableInfo {
     int table[9][9];
+    int fixed[9][9];
     int candidates[9][9][9];
 };
 
@@ -9,5 +16,7 @@ class SudokuTable{
     public:
         tableInfo tableInfo;
         void getTable();
+        void getFixed();
         void getCandidates();
+        void countFilled();
 };
